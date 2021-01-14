b1e0333dbfe71356208d0498f24b565b45f44017
Triansh <trianshsharma@gmail.com>
2021-01-14 10:02:11 +0530

    [Setup] Frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './App';
+import reducers from './reducers';
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+const middleware = [thunk];
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(reducers, compose(applyMiddleware(...middleware), reduxDevTools));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
